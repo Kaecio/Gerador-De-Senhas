a37ba273649e0cbce6dc556715506211c9e47980
Kaecio <56613892+Kaecio@users.noreply.github.com>
2020-10-17 01:10:41 -0300

    Inserindo códigos

--- GeradorDeSenhas/Form1.cs
+++ GeradorDeSenhas/Form1.cs
@@ -12,7 +12,8 @@ namespace GeradorDeSenhas
 {
     public partial class Form1 : Form
     {
-        int password_num = 10;
+        int password_num = 20;
+        
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +21,37 @@ namespace GeradorDeSenhas
 
         private void btnGerar_Click(object sender, EventArgs e)
         {
-            string chars = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+            string alfabeto = "abcdefghijklmnopqrstuvwxyz";
 
-        }
+            if (checkMaiuscula.Checked)
+            {
+                alfabeto += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+            else if (checkAlgarismo.Checked)
+            {
+                alfabeto += "0123456789";
+            }
+            else if (checkSimbolos.Checked)
+            {
+                alfabeto += "!@#$%&*+";
+            }
+            Lista_senhas.Items.Clear();
+
+            Random gerarNum = new Random();
+            for (int g = 1; g < password_num ; g++)
+            {
+                StringBuilder str = new StringBuilder();
+                for (int i = 1; i <= count_chars.Value; i++)
+                {
+                    int posicao = gerarNum.Next(0, alfabeto.Length);
+                    str.Append(alfabeto[posicao].ToString());
+
+                }
+                Lista_senhas.Items.Add(str.ToString());
+            }
+
+            
+         }
 
         
     }
